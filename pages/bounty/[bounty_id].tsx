d4c5f030df3358d14ff8456f6187eea057a6a508
Michael Krukar <krukar@gmail.com>
2023-01-29 13:02:42 -0500

    Added try catch to bounty page

--- pages/bounty/[bounty_id].tsx
+++ pages/bounty/[bounty_id].tsx
@@ -128,96 +128,74 @@ const get_winning_claim = (claims: Bounty["claims"], winning_claim_id: Claim["id
 };
 
 export async function getStaticProps({ params: { bounty_id } }: { params: { bounty_id: Bounty["id"] } }) {
-    const { Item } = await aws.dynamo.get_item({
-        TableName: "turboardio_bounties",
-        Key: {
-            bounty_id: aws.dynamo.input(bounty_id),
-        },
-    });
+    try {
+        const { Item } = await aws.dynamo.get_item({
+            TableName: "turboardio_bounties",
+            Key: {
+                bounty_id: aws.dynamo.input(bounty_id),
+            },
+        });
 
-    const { admin_id, claim_id, created_at, details, game_id } = aws.dynamo.unmarshall(Item);
+        const { admin_id, claim_id, created_at, details, game_id } = aws.dynamo.unmarshall(Item);
 
-    const admin = await get_user(admin_id);
+        const admin = await get_user(admin_id);
 
-    const game = await get_game(game_id);
+        const game = await get_game(game_id);
 
-    const pledges = await get_pledges(bounty_id);
+        const pledges = await get_pledges(bounty_id);
 
-    const prize = pledges ? pledges.reduce((acc, { amount }) => acc + amount, 0) : null;
+        const prize = pledges ? pledges.reduce((acc, { amount }) => acc + amount, 0) : null;
 
-    const claims = await get_claims(bounty_id);
+        const claims = await get_claims(bounty_id);
 
-    const winning_claim = get_winning_claim(claims, claim_id);
+        const winning_claim = get_winning_claim(claims, claim_id);
 
-    let meta_description = `${game.title} bounty.`;
+        let meta_description = `${game.title} bounty.`;
 
-    if (prize) {
-        meta_description = ` Prize: ${prize}`;
+        if (prize) {
+            meta_description = ` Prize: ${prize}`;
+        }
+
+        if (winning_claim) {
+            meta_description += ` Bounty has been claimed by ${winning_claim.user.name}.`;
+        }
+
+        const props: BountyProps = {
+            bounty: {
+                admin,
+                claims,
+                created_at: format.created_at(created_at),
+                details,
+                game,
+                id: bounty_id,
+                pledges,
+                prize,
+                winning_claim,
+            },
+            meta: {
+                description: meta_description,
+                title: `${game.title} Bounty`,
+                url: `https://turboboard.io/bounty/${bounty_id}`,
+            },
+        };
+
+        return {
+            props,
+            revalidate: 1,
+        };
+    } catch (err) {
+        console.log("ERR", err);
+
+        return {
+            props: {},
+            revalidate: 1,
+        };
     }
-
-    if (winning_claim) {
-        meta_description += ` Bounty has been claimed by ${winning_claim.user.name}.`;
-    }
-
-    const props: BountyProps = {
-        bounty: {
-            admin,
-            claims,
-            created_at: format.created_at(created_at),
-            details,
-            game,
-            id: bounty_id,
-            pledges,
-            prize,
-            winning_claim,
-        },
-        meta: {
-            description: meta_description,
-            title: `${game.title} Bounty`,
-            url: `https://turboboard.io/bounty/${bounty_id}`,
-        },
-    };
-
-    return {
-        props,
-        revalidate: 1,
-    };
 }
 
 export async function getStaticPaths() {
-    const ids = [
-        "4TUKKmUst_e5HyLQEdJfF",
-        "jO5MSBVju7d6yzOQn7SnF",
-        "v5Ocf4GFK17CONWpDJJuA",
-        "_9jszQdNXA4dj29MBRdv8",
-        "pN9i0wxy1KpYoUqZVItHB",
-        "HkP_VmOleFJUhRP_6gsJg",
-        "MEq2JSfPz2ef8208S4FNF",
-        "ESjLnX5ptZSM_XvUOngvN",
-        "RO7SAma9kLsSs_EV6HLrL",
-        "lwbflmzJkttRa7a9pcL15",
-        "oOPH4S8bL-V9ZKR-3zsFv",
-        "5DeqTY4JzhKqmFTLogTRI",
-        "WVi2hWe9sjAvDE8F_1CYG",
-        "qoLVWOLsdBGd2pKgwMcTo",
-        "RGmWQfQiunX19nwQjRIMQ",
-        "fDa-SmNMIfg556k68NfVt",
-        "'-mBfc6h-Nxoz7j55mCzNd",
-        "UpS4KAz4RZohdVOneJxv4",
-        "kdDwm1B194DqxkYYyO8Aq",
-        "s6g7R6GkaxZLdQtqAV4fD",
-        "'-dKsdt4-MbKvd7cKVAsby",
-        "J8AWNiXTrC3MSFwQbx6af",
-    ];
-
-    const paths = ids.map((bounty_id) => ({
-        params: {
-            bounty_id,
-        },
-    }));
-
     return {
-        paths,
+        paths: [],
         fallback: true,
     };
 }
