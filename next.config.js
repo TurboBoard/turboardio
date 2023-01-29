bcb4350eb44e5f1b54fbde7b93e53084bc7d6d9a
Michael Krukar <krukar@gmail.com>
2023-01-29 13:40:17 -0500

    Disabled multithreading

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    experimental: {
+        // This is experimental but can
+        // be enabled to allow parallel threads
+        // with nextjs automatic static generation
+        workerThreads: false,
+        cpus: 1,
+    },
+};
+
+module.exports = nextConfig;
