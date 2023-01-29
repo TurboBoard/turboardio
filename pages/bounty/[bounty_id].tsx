b07d3a2dac478019100460db2b0da0117048bfda
Michael Krukar <krukar@gmail.com>
2023-01-29 13:19:58 -0500

    Testing slim bounty page

--- pages/bounty/[bounty_id].tsx
+++ pages/bounty/[bounty_id].tsx
@@ -13,35 +13,35 @@ const Page = (props: BountyProps) => {
     return <Layout {...props} />;
 };
 
-const get_claims = async (bounty_id: Bounty["id"]): Promise<Bounty["claims"]> => {
-    const { Items } = await aws.dynamo.scan({
-        TableName: "turboardio_claims",
-        FilterExpression: "bounty_id = :bounty_id",
-        ExpressionAttributeValues: {
-            ":bounty_id": aws.dynamo.input(bounty_id),
-        },
-    });
+// const get_claims = async (bounty_id: Bounty["id"]): Promise<Bounty["claims"]> => {
+//     const { Items } = await aws.dynamo.scan({
+//         TableName: "turboardio_claims",
+//         FilterExpression: "bounty_id = :bounty_id",
+//         ExpressionAttributeValues: {
+//             ":bounty_id": aws.dynamo.input(bounty_id),
+//         },
+//     });
 
-    if (!Items.length) return null;
+//     if (!Items.length) return null;
 
-    const claims = [];
+//     const claims = [];
 
-    const sorted = Items.map((Item) => aws.dynamo.unmarshall(Item)).sort((a, b) => new Date(b.created_at).valueOf() - new Date(a.created_at).valueOf());
+//     const sorted = Items.map((Item) => aws.dynamo.unmarshall(Item)).sort((a, b) => new Date(b.created_at).valueOf() - new Date(a.created_at).valueOf());
 
-    for (const { comment, created_at, claim_id, link, user_id } of sorted) {
-        const user = await get_user(user_id);
+//     for (const { comment, created_at, claim_id, link, user_id } of sorted) {
+//         const user = await get_user(user_id);
 
-        claims.push({
-            comment: comment || null,
-            created_at: format.created_at(created_at),
-            id: claim_id,
-            link,
-            user,
-        });
-    }
+//         claims.push({
+//             comment: comment || null,
+//             created_at: format.created_at(created_at),
+//             id: claim_id,
+//             link,
+//             user,
+//         });
+//     }
 
-    return claims;
-};
+//     return claims;
+// };
 
 const get_game = async (game_id: Game["id"]): Promise<Game> => {
     const response = await fetch(process.env.IGDB_GAME_ENDPOINT, {
@@ -58,31 +58,31 @@ const get_game = async (game_id: Game["id"]): Promise<Game> => {
     return await response.json();
 };
 
-const get_pledges = async (bounty_id: Bounty["id"]): Promise<Bounty["pledges"]> => {
-    const { Items } = await aws.dynamo.scan({
-        TableName: "turboardio_pledges",
-        FilterExpression: "bounty_id = :bounty_id",
-        ExpressionAttributeValues: {
-            ":bounty_id": aws.dynamo.input(bounty_id),
-        },
-    });
+// const get_pledges = async (bounty_id: Bounty["id"]): Promise<Bounty["pledges"]> => {
+//     const { Items } = await aws.dynamo.scan({
+//         TableName: "turboardio_pledges",
+//         FilterExpression: "bounty_id = :bounty_id",
+//         ExpressionAttributeValues: {
+//             ":bounty_id": aws.dynamo.input(bounty_id),
+//         },
+//     });
 
-    if (!Items.length) return null;
+//     if (!Items.length) return null;
 
-    const pledges = [];
+//     const pledges = [];
 
-    for (const Item of Items) {
-        const { amount, pledge_id, user_id } = aws.dynamo.unmarshall(Item);
+//     for (const Item of Items) {
+//         const { amount, pledge_id, user_id } = aws.dynamo.unmarshall(Item);
 
-        pledges.push({
-            amount,
-            id: pledge_id,
-            user_id,
-        });
-    }
+//         pledges.push({
+//             amount,
+//             id: pledge_id,
+//             user_id,
+//         });
+//     }
 
-    return pledges;
-};
+//     return pledges;
+// };
 
 const get_user = async (admin_id: TurboardioUser["id"]): Promise<TurboardioUser> => {
     const { Item } = await aws.dynamo.get_item({
@@ -95,37 +95,37 @@ const get_user = async (admin_id: TurboardioUser["id"]): Promise<TurboardioUser>
     return convert.turboardio_user(aws.dynamo.unmarshall(Item));
 };
 
-const get_winning_claim = (claims: Bounty["claims"], winning_claim_id: Claim["id"]): Bounty["winning_claim"] => {
-    if (!winning_claim_id) return null;
+// const get_winning_claim = (claims: Bounty["claims"], winning_claim_id: Claim["id"]): Bounty["winning_claim"] => {
+//     if (!winning_claim_id) return null;
 
-    const winning_claim = claims.find(({ id }) => id === winning_claim_id);
+//     const winning_claim = claims.find(({ id }) => id === winning_claim_id);
 
-    const youtube_id = convert.link_to_youtube_id(winning_claim.link);
+//     const youtube_id = convert.link_to_youtube_id(winning_claim.link);
 
-    if (youtube_id) {
-        return {
-            ...winning_claim,
-            video: {
-                id: youtube_id,
-                type: "youtube",
-            },
-        };
-    }
+//     if (youtube_id) {
+//         return {
+//             ...winning_claim,
+//             video: {
+//                 id: youtube_id,
+//                 type: "youtube",
+//             },
+//         };
+//     }
 
-    const twitch_id = convert.link_to_twitch_id(winning_claim.link);
+//     const twitch_id = convert.link_to_twitch_id(winning_claim.link);
 
-    if (twitch_id) {
-        return {
-            ...winning_claim,
-            video: {
-                id: twitch_id,
-                type: "twitch",
-            },
-        };
-    }
+//     if (twitch_id) {
+//         return {
+//             ...winning_claim,
+//             video: {
+//                 id: twitch_id,
+//                 type: "twitch",
+//             },
+//         };
+//     }
 
-    return winning_claim;
-};
+//     return winning_claim;
+// };
 
 export async function getStaticProps({ params: { bounty_id } }: { params: { bounty_id: Bounty["id"] } }) {
     try {
@@ -142,35 +142,35 @@ export async function getStaticProps({ params: { bounty_id } }: { params: { boun
 
         const game = await get_game(game_id);
 
-        const pledges = await get_pledges(bounty_id);
+        // const pledges = await get_pledges(bounty_id);
 
-        const prize = pledges ? pledges.reduce((acc, { amount }) => acc + amount, 0) : null;
+        // const prize = pledges ? pledges.reduce((acc, { amount }) => acc + amount, 0) : null;
 
-        const claims = await get_claims(bounty_id);
+        // const claims = await get_claims(bounty_id);
 
-        const winning_claim = get_winning_claim(claims, claim_id);
+        // const winning_claim = get_winning_claim(claims, claim_id);
 
         let meta_description = `${game.title} bounty.`;
 
-        if (prize) {
-            meta_description = ` Prize: ${prize}`;
-        }
+        // if (prize) {
+        //     meta_description = ` Prize: ${prize}`;
+        // }
 
-        if (winning_claim) {
-            meta_description += ` Bounty has been claimed by ${winning_claim.user.name}.`;
-        }
+        // if (winning_claim) {
+        //     meta_description += ` Bounty has been claimed by ${winning_claim.user.name}.`;
+        // }
 
         const props: BountyProps = {
             bounty: {
                 admin,
-                claims,
+                claims: null,
                 created_at: format.created_at(created_at),
                 details,
                 game,
                 id: bounty_id,
-                pledges,
-                prize,
-                winning_claim,
+                pledges: null,
+                prize: null,
+                winning_claim: null,
             },
             meta: {
                 description: meta_description,
